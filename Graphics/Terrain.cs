b684d054c2c9f995896168984b3ef35d1ed98303
Altofy <lotfy8170@gmail.com>
2020-12-28 15:56:52 +0200

    terrain creation

--- Graphics/Terrain.cs
+++ Graphics/Terrain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace Graphics
 {
     class Terrain
     {
+        private Bitmap heightMap;
+        int [,]terrain;
+
+
+        public Terrain(String path)
+        {
+            heightMap = new Bitmap(path);
+            terrain = new int [heightMap.Height , heightMap.Width];
+            for (int i = 0; i < heightMap.Height; i++)
+            {
+                for (int j = 0; j < heightMap.Width; j++)
+                {
+                    terrain[i, j] = heightMap.GetPixel(i,j).G;
+                }
+            }
+            
+        }
     }
 }
